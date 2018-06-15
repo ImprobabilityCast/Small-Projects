9bc3e46894ef636a30ca36e6fee08cb6d61db020
ImprobabilityCast <segmentationfault3@outlook.com>
2018-06-14 23:15:48 -0400

    first printing attempt

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Printing;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +16,20 @@ namespace GraderTools
     /// </summary>
     public partial class App : Application
     {
+        App()
+        {
+            LocalPrintServer localPrintServer = new LocalPrintServer();
+            PrintQueue printQueue = localPrintServer.DefaultPrintQueue;
+
+            PrintSystemJobInfo info = printQueue.AddJob("SW Grading");
+
+            StreamReader reader = new StreamReader("C:\\repo\\GraderTools\\App.xaml.cs");
+            Byte[] contents = UnicodeEncoding.Unicode.GetBytes(reader.ReadToEnd());
+            reader.Close();
+
+            Stream jobStream = info.JobStream;
+            jobStream.Write(contents, 0, contents.Length);
+            jobStream.Close();
+        }
     }
 }
